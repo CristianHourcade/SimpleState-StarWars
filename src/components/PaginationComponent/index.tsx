150b0d79e84e66f73fdffe023810b3b7476c58c9
Cristian Hourcade <hourcadecristian@gmail.com>
2021-08-04 01:16:24 -0300

    Fix: Add validation to PainationComponent

--- src/components/PaginationComponent/index.tsx
+++ src/components/PaginationComponent/index.tsx
@@ -1,16 +1,19 @@
 import { PaginationComponentProps } from "../../models/components/pagination";
 
-const PaginationComponent = (props: PaginationComponentProps) => {
+const PaginationComponent = (props: PaginationComponentProps): JSX.Element => {
     return (
-        Array(props.navigationData.count / 10).fill(true).map((_, index) => {
-            return (
-                <span key={index}>
-                    <a href="#"
-                        onClick={() => props.changePage(index + 1)}
-                        className={(index + 1 == props.pageIndex) ? 'pagination-active' : ''}>{index + 1}</a>
-                </span>
-            )
-        })
+        <>
+            {Array(props.navigationData.count / 10).fill(true).map((_, index) => {
+                return (
+                    <span key={index}>
+                        <a href="#"
+                            onClick={() => props.changePage(index + 1)}
+                            className={(index + 1 == props.pageIndex) ? 'pagination-active' : ''}>{index + 1}</a>
+                    </span>
+                )
+            })
+            }
+        </>
     )
 }
 export default PaginationComponent;
